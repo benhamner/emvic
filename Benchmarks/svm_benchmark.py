3a00997442f38944e8ed5ca27fc55e12def02306
Ben Hamner <ben@benhamner.com>
2012-03-19 23:34:51 -0400

    Added SVM benchmark

--- Benchmarks/svm_benchmark.py
+++ Benchmarks/svm_benchmark.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from sklearn.ensemble import RandomForestClassifier
+from sklearn import svm
 import csv_io
 
 def main():
@@ -10,13 +10,13 @@ def main():
     test, throwaway = csv_io.read_data("../Data/test.csv")
     test = [x[1:] for x in test]
 
-    rf = RandomForestClassifier(n_estimators=100, min_split=2)
-    rf.fit(training, target)
-    predicted_probs = rf.predict_proba(test)
+    svc = svm.SVC(probability=True)
+    svc.fit(training, target)
+    predicted_probs = svc.predict_proba(test)
     predicted_probs = [[min(max(x,0.001),0.999) for x in y] 
                        for y in predicted_probs]
     predicted_probs = [["%f" % x for x in y] for y in predicted_probs]
-    csv_io.write_delimited_file("../Submissions/rf_benchmark.csv",
+    csv_io.write_delimited_file("../Submissions/svm_benchmark.csv",
                                 predicted_probs)
 
 if __name__=="__main__":
